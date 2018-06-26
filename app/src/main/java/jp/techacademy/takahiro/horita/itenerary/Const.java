51591cc5213950bdd9f27bd8700b3e60820762ec
TakahiroHorita <37589728+TakahiroHorita@users.noreply.github.com>
2018-06-26 14:53:42 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takahiro/horita/itenerary/Const.java
@@ -0,0 +1,12 @@
+package jp.techacademy.takahiro.horita.itenerary;
+
+//定数用クラス
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+
+    public static final String FavoritesPATH = "favorites";
+}
